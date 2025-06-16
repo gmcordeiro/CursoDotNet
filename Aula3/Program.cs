8552f8646e77a53ce1d05d1b31cf6b9bea0b172d
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2025-06-16 17:21:33 -0300

    Tipos, variaveis, constantes e valiáveis dinâmicas

--- Aula3/Program.cs
+++ Aula3/Program.cs
@@ -16,12 +16,33 @@ public class Program
         Console.WriteLine($"Nome: {MY_NAME}, Aniversário: {myBirthday}");
 
 
-        // Conteúdo da aula
+        /*
+         * Conteúdo da aula 3
+         */
+
+        // Const
         const int NUMBER = 30;
 
         // NUMBER = 20; Da erro de compilação, pois NUMBER é uma constante e não pode ser alterada
 
-        Console.WriteLine("Número é: " + NUMBER);
+        Console.WriteLine("valor do número é: " + NUMBER);
+
+
+        // Var
+        var valiable = "Guilherrme M. Cordeiro";
+
+        // var é usado para declarar uma variável cujo tipo é inferido pelo compilador. O Valor precisa ser atribuído no momento da declaração, e o tipo não pode ser alterado posteriormente.
+        // valiable = 50; // Isso não é válido, pois var é tipado, porém pelo compilador
+
+        Console.WriteLine("Valor da variável é: " + valiable);
+
+        // Dynamic
+        dynamic dynamicVariable = "Guilherme M. Cordeiro";
+
+        // dynamic é usado para declarar uma variável cujo tipo pode ser alterado em tempo de execução
+        dynamicVariable = 50; // Isso é válido, pois dynamic permite qualquer tipo de dado
+
+        Console.WriteLine("Valor da variável dinâmica é: " + dynamicVariable);
     }
 
 }
