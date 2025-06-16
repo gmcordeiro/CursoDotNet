94f32b3a576423090c76c9c462c7ec47ed7ed9c7
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2025-06-16 19:25:36 -0300

    Aula 9 - Operadores lógicos (AND, OR, NOT, etc)

---
+++ Aula9/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Aula9;
+
+public class Program {
+	public static void Main() {
+
+		// Aula 9 - Operadores lógicos
+		Console.WriteLine("============Operadores lógicos============");
+
+		bool isLogged = true;
+		bool hasAdminAccess = false;
+
+		Console.WriteLine($"Informações do uauário: ");
+		Console.WriteLine($"Usuário logado: {isLogged}");
+		Console.WriteLine($"Acesso ao painel de administrador: {hasAdminAccess}");
+
+		// Operador lógico AND (&&)
+		if (isLogged && hasAdminAccess) {
+			Console.WriteLine("Usuário logado e com acesso ao painel de administrador.");
+		} else if (isLogged && !hasAdminAccess) {
+			Console.WriteLine("Usuário logado, mas sem acesso ao painel de administrador.");
+		} else {
+			Console.WriteLine("Usuário não está logado.");
+		}
+
+	}
+}
