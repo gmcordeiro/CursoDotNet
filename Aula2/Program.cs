a45fcd480135db844507e86f3b32aef835914881
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2025-06-16 16:54:31 -0300

    C

--- Aula2/Program.cs
+++ Aula2/Program.cs
@@ -11,5 +11,14 @@ public class Program
          * Exemplo de comentário em bloco, usado para documentação ou explicações mais longas.
          */
         Console.WriteLine("Bem-vindo ao C#!");
+
+        // Aula sobre variaveis
+
+        string myName = "Guilherme M. Cordeiro";
+        string myBirthday = "15/02/1996";
+        Console.WriteLine($"Nome: {myName}, Aniversário: {myBirthday}");
+
+
+
     }
 }
