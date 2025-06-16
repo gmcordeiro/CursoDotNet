6fe4089ca1f48277d6590ad1f444039c8bda8b02
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2025-06-16 20:19:14 -0300

    Aula 14 - Exemplo de for

---
+++ Aula14/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula14;
+
+// For loop example
+public class Program {
+	public static void Main() {
+		Console.WriteLine("Enter a number between 1 and 5 (or 'exit' to quit):");
+		int input = Convert.ToInt32(Console.ReadLine());
+		for (int i = 0; i <= input; i++) {
+			Console.WriteLine($"Count: {i}");
+		}
+	}
+}
