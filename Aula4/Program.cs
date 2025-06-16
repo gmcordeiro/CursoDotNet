600762250dc47402e682ebb29fb9bca1e32cc93d
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2025-06-16 17:37:05 -0300

    Preenchimento de formulário

--- Aula4/Program.cs
+++ Aula4/Program.cs
@@ -7,5 +7,18 @@ public class Program
         /*
          * Aula sobre INPUT e OUTPUT
          */
+
+        Console.WriteLine("----------------FORM----------------");
+
+        Console.WriteLine("Digite o seu nome: ");
+        string? name = Console.ReadLine();
+
+        Console.WriteLine("Digite a sua idade: ");
+        int ageInput = Convert.ToInt32(Console.ReadLine() ?? "0");
+
+        Console.WriteLine($"Olá, {name}!");
+        Console.WriteLine($"Sua idade é: {ageInput}");
+
+
     }
 }
