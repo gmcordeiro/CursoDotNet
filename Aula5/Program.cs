e8536c9047c3b829a381b1c08035e80ec366e31c
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2025-06-16 18:04:05 -0300

    Aula de operadores numéricos

---
+++ Aula5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Aula5;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Operadores aritméticos
+        Console.WriteLine("==========Operadores Aritméticos em C#==========");
+        Console.WriteLine("Digite o primeiro numero: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite o segundo numero: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = number1 + number2; // Suma
+        int sub = number1 - number2; // Subtração
+        int mul = number1 * number2; // Multiplicação
+        int div = number1 / number2; // Divisão
+        int mod = number1 % number2; // Módulo (resto da divisão)
+
+        Console.WriteLine($"Suma: {sum}");
+        Console.WriteLine($"Subtração: {sub}");
+        Console.WriteLine($"Multiplicação: {mul}");
+        Console.WriteLine($"Divisão: {div}");
+        Console.WriteLine($"Resto: {mod}");
+    }
+}
