753e55d3cf6043f0eb456678a946d63e499a251e
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2025-06-16 20:06:12 -0300

    Mudei o nome das pastas e dos arquivos dos projetos. Criei a aula 11 com exemplo de switch.

---
+++ Aula11/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula11;
+
+public class Program {
+	public static void Main(string[] args) {
+
+		// inline Switch expression example
+		Console.WriteLine("Enter a number between 1 and 5:");
+		int umber = Console.ReadLine() switch {
+			"1" => 1,
+			"2" => 2,
+			"3" => 3,
+			"4" => 4,
+			"5" => 5,
+			_ => throw new ArgumentException("Invalid number!")
+		};
+
+		Console.WriteLine($"You entered: {umber}");
+
+	}
+}
