49cef112d324762aca410626f67f2b40ca3e4b02
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2025-06-16 16:48:51 -0300

    Aula 2 - Commit inicial

---
+++ Aula2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula2;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Exemplo de comentário em linha única
+        Console.WriteLine("Olá, Mundo!");
+
+        /*
+         * Exemplo de comentário em bloco, usado para documentação ou explicações mais longas.
+         */
+        Console.WriteLine("Bem-vindo ao C#!");
+    }
+}
