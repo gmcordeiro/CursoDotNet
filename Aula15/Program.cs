f57285ced6cf3a7807c86050c96cc03da1b839e4
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2025-06-16 20:37:22 -0300

    exemplos de foreach

---
+++ Aula15/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula15;
+
+public class Program {
+	public static void Main() {
+
+		// Aula 15, exemplo de foreach loop e interpolação de strings
+		List<int> ints = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		foreach (int number in ints) {
+			Console.WriteLine($"Number: {number}");
+		}
+
+
+	}
+
+}
